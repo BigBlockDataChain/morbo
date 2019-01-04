d764337dc58b7f81b16287a0f5b888890ca51876
francium <francium@francium.cc>
2019-01-04 18:39:40 -0500

    Refactor Mirror Mark prototype based definition to an ES6 class

--- src/components/editor/mirror-mark/index.js
+++ src/components/editor/mirror-mark/index.js
@@ -1,37 +1,131 @@
 /* tslint:disable */
-import * as CodeMirror from 'codemirror'
-
-import './styles.css'
-
 /**
  * @license MirrorMark v0.1
  * (c) 2015 Musicbed http://www.musicbed.com
  * License: MIT
  */
 
-/**
- * Our delegate prototype used by our factory
- * @type {Object}
- */
-const mirrorMarkProto = {
+import * as CodeMirror from 'codemirror'
+
+import './styles.css'
+
+class MirrorMark {
+
+  constructor(element, options) {
+    this.element = element
+    this.options = options
+
+    this.tools = [
+      {name: 'bold', action: 'bold', className: 'fa fa-bold'},
+      {name: 'italicize', action: 'italicize', className: 'fa fa-italic'},
+      {name: 'blockquote', action: 'blockquote', className: 'fa fa-quote-left'},
+      {name: 'link', action: 'link', className: 'fa fa-link'},
+      {name: 'image', action: 'image', className: 'fa fa-image'},
+      {name: 'unorderedList', action: 'unorderedList', className: 'fa fa-list'},
+      {name: 'orderedList', action: 'orderedList', className: 'fa fa-list-ol'},
+      {name: 'fullScreen', action: 'fullScreen', className: 'fa fa-expand'},
+    ]
+
+    this.keyMaps = {
+      // NOTE: For now remember to register Mac (Cmd-) versions too
+      'Ctrl-B': 'bold',
+      'Ctrl-I': 'italicize',
+      'Ctrl-\'': 'blockquote',
+      'Ctrl-Alt-L': 'orderedList',
+      'Ctrl-L': 'unorderedList',
+      'Ctrl-Alt-I': 'image',
+      'Ctrl-H': 'hr',
+      'Ctrl-K': 'link',
+      // TODO: Reduce this duplication and automate setup of both Ctrl and Cmd prefixes
+      'Cmd-B': 'bold',
+      'Cmd-I': 'italicize',
+      'Cmd-\'': 'blockquote',
+      'Cmd-Alt-L': 'orderedList',
+      'Cmd-L': 'unorderedList',
+      'Cmd-Alt-I': 'image',
+      'Cmd-H': 'hr',
+      'Cmd-K': 'link',
+    }
+
+    this.actions = {
+      bold: function () {
+        this.insertAround('**', '**')
+      },
+      italicize: function () {
+        this.insertAround('*', '*')
+      },
+      code: function () {
+        this.insertAround('```\r\n', '\r\n```')
+      },
+      blockquote: function () {
+        this.insertBefore('> ', 2)
+      },
+      orderedList: function () {
+        this.insertBefore('1. ', 3)
+      },
+      unorderedList: function () {
+        this.insertBefore('* ', 2)
+      },
+      image: function () {
+        this.insertBefore('![](http://)', 2)
+      },
+      link: function () {
+        this.insertAround('[', '](http://)')
+      },
+      hr: function () {
+        this.insert('---')
+      },
+      fullScreen: function () {
+        const el = this.cm.getWrapperElement()
+
+        // https://developer.mozilla.org/en-US/docs/DOM/Using_fullscreen_mode
+        const doc = document
+        const isFull = doc.fullScreen || doc.mozFullScreen || doc.webkitFullScreen
+        const request = function() {
+          if (el.requestFullscreen) {
+            el.requestFullscreen()
+          } else if (el.webkitRequestFullscreen) {
+            el.webkitRequestFullscreen()
+          } else if (el.mozRequestFullScreen) {
+            el.mozRequestFullScreen()
+          } else if (el.msRequestFullscreen) {
+            el.msRequestFullscreen()
+          }
+        }
+        const cancel = function() {
+          if (doc.cancelFullScreen) {
+            doc.cancelFullScreen()
+          } else if (doc.mozCancelFullScreen) {
+            doc.mozCancelFullScreen()
+          } else if (doc.webkitCancelFullScreen) {
+            doc.webkitCancelFullScreen()
+          }
+        }
+        if (!isFull) {
+          request()
+        } else if (cancel) {
+          cancel()
+        }
+      }
+    }
+  }
 
   /**
    * Render the component
    */
-  render: function render() {
+  render() {
     this.registerKeyMaps(this.keyMaps)
     this.cm = CodeMirror.fromTextArea(this.element, this.options)
 
     if (this.options.showToolbar) {
       this.setToolbar(this.tools)
     }
-  },
+  }
 
   /**
    * Setup the toolbar
    */
-  setToolbar: function setToolbar(tools) {
-
+  setToolbar(tools) {
     const toolbar = document.createElement('ul')
     toolbar.className = this.options.theme + '-toolbar'
 
@@ -42,14 +136,13 @@ const mirrorMarkProto = {
 
     const cmWrapper = this.cm.getWrapperElement()
     cmWrapper.parentNode.insertBefore(toolbar, cmWrapper)
-
-  },
+  }
 
   /**
    * Register Keymaps by extending the extraKeys object
    * @param {Object} keyMaps
    */
-  registerKeyMaps: function registerKeyMaps(keyMaps) {
+  registerKeyMaps(keyMaps) {
     for (const name in keyMaps) {
       if (typeof(this.actions[keyMaps[name]]) !== 'function')
         throw 'MirrorMark - \'' + keyMaps[name] + '\' is not a registered action'
@@ -58,17 +151,15 @@ const mirrorMarkProto = {
       obj[name] = this.actions[keyMaps[name]].bind(this)
       Object.assign(this.options.extraKeys, obj)
     }
-  },
-
+  }
 
   /**
    * Register actions by extending the default actions
    * @param  {Object} actions [description]
    */
-  registerActions: function registerActions(actions) {
+  registerActions(actions) {
     return Object.assign(this.actions, actions)
-  },
-
+  }
 
   /**
    * Register tools by extending and overwriting the default tools
@@ -76,7 +167,7 @@ const mirrorMarkProto = {
    * @param  {Bool} replace - replace the default tools with the ones provided. Defaults
    *   to false.
    */
-  registerTools: function registerTools(tools, replace) {
+  registerTools(tools, replace) {
     for (const action in tools) {
       if (this.actions[tools[action].action]
           && typeof(this.actions[tools[action].action]) !== 'function')
@@ -89,13 +180,13 @@ const mirrorMarkProto = {
     }
 
     this.tools = this.tools.concat(tools)
-  },
+  }
 
   /**
    * A recursive function to generate and return an unordered list of tools
    * @param  {Object}
    */
-  generateToolList: function generateToolList(tools) {
+  generateToolList(tools) {
     return tools.map(function(tool) {
       const item = document.createElement('li'),
         anchor = document.createElement('a')
@@ -135,121 +226,25 @@ const mirrorMarkProto = {
       return item
 
     }.bind(this))
-  },
-
-  /**
-   * Default Tools in Toolbar
-   * @todo - update so it's not so tightly coupled with Font Awesome.
-   */
-  tools: [
-    {name: 'bold', action: 'bold', className: 'fa fa-bold'},
-    {name: 'italicize', action: 'italicize', className: 'fa fa-italic'},
-    {name: 'blockquote', action: 'blockquote', className: 'fa fa-quote-left'},
-    {name: 'link', action: 'link', className: 'fa fa-link'},
-    {name: 'image', action: 'image', className: 'fa fa-image'},
-    {name: 'unorderedList', action: 'unorderedList', className: 'fa fa-list'},
-    {name: 'orderedList', action: 'orderedList', className: 'fa fa-list-ol'},
-    {name: 'fullScreen', action: 'fullScreen', className: 'fa fa-expand'},
-  ],
-
-  /**
-   * Default Keymaps
-   * @type {Object}
-   */
-  keyMaps: {
-    'Ctrl-B': 'bold',
-    'Ctrl-I': 'italicize',
-    'Ctrl-\'': 'blockquote',
-    'Ctrl-Alt-L': 'orderedList',
-    'Ctrl-L': 'unorderedList',
-    'Ctrl-Alt-I': 'image',
-    'Ctrl-H': 'hr',
-    'Ctrl-K': 'link',
-  },
-
-  /**
-   * Default Actions
-   * @type {Object}
-   */
-  actions: {
-    bold: function () {
-      this.insertAround('**', '**')
-    },
-    italicize: function () {
-      this.insertAround('*', '*')
-    },
-    code: function () {
-      this.insertAround('```\r\n', '\r\n```')
-    },
-    blockquote: function () {
-      this.insertBefore('> ', 2)
-    },
-    orderedList: function () {
-      this.insertBefore('1. ', 3)
-    },
-    unorderedList: function () {
-      this.insertBefore('* ', 2)
-    },
-    image: function () {
-      this.insertBefore('![](http://)', 2)
-    },
-    link: function () {
-      this.insertAround('[', '](http://)')
-    },
-    hr: function () {
-      this.insert('---')
-    },
-    fullScreen: function () {
-      const el = this.cm.getWrapperElement()
-
-      // https://developer.mozilla.org/en-US/docs/DOM/Using_fullscreen_mode
-      const doc = document
-      const isFull = doc.fullScreen || doc.mozFullScreen || doc.webkitFullScreen
-      const request = function() {
-        if (el.requestFullscreen) {
-          el.requestFullscreen()
-        } else if (el.webkitRequestFullscreen) {
-          el.webkitRequestFullscreen()
-        } else if (el.mozRequestFullScreen) {
-          el.mozRequestFullScreen()
-        } else if (el.msRequestFullscreen) {
-          el.msRequestFullscreen()
-        }
-      }
-      const cancel = function() {
-        if (doc.cancelFullScreen) {
-          doc.cancelFullScreen()
-        } else if (doc.mozCancelFullScreen) {
-          doc.mozCancelFullScreen()
-        } else if (doc.webkitCancelFullScreen) {
-          doc.webkitCancelFullScreen()
-        }
-      }
-      if (!isFull) {
-        request()
-      } else if (cancel) {
-        cancel()
-      }
-    }
-  },
+  }
 
   /**
    * Insert a string at cursor position
    * @param  {String} insertion
    */
-  insert: function insert(insertion) {
+  insert(insertion) {
     const doc = this.cm.getDoc()
     const cursor = doc.getCursor()
 
     doc.replaceRange(insertion, { line: cursor.line, ch: cursor.ch })
-  },
+  }
 
   /**
    * Insert a string at the start and end of a selection
    * @param  {String} start
    * @param  {String} end
    */
-  insertAround: function insertAround(start, end) {
+  insertAround(start, end) {
     const doc = this.cm.getDoc()
     const cursor = doc.getCursor()
 
@@ -262,13 +257,13 @@ const mirrorMarkProto = {
       doc.replaceRange(start + end, {line: cursor.line, ch: cursor.ch})
       doc.setCursor({line: cursor.line, ch: cursor.ch + start.length})
     }
-  },
+  }
 
   /**
    * Insert a string before a selection
    * @param  {String} insertion
    */
-  insertBefore: function insertBefore(insertion, cursorOffset) {
+  insertBefore(insertion, cursorOffset) {
     const doc = this.cm.getDoc()
     const cursor = doc.getCursor()
 
@@ -288,16 +283,16 @@ const mirrorMarkProto = {
       doc.setCursor({ line: cursor.line, ch: cursorOffset || 0 })
     }
   }
+
 }
 
 /**
- * Our Factory
+ * Factory
  * @param  {Object} element
  * @param  {Object} options
  * @return {Object}
  */
 export default function mirrorMark(element, options) {
-
   // Defaults
   const defaults = {
     theme: 'mirrormark',
@@ -310,15 +305,12 @@ export default function mirrorMark(element, options) {
     mode: 'markdown',
   }
 
-  return Object.assign(
-    Object.create(mirrorMarkProto),
+  return new MirrorMark(
+    element,
     {
-      element,
-      options: {
-        // Extend our defaults with the options provided
-        ...defaults,
-        ...options,
-      },
+      // Extend our defaults with the options provided
+      ...defaults,
+      ...options,
     },
   )
 }
