4aceec01a6a650ab0e65e110dbf82887696aa99d
francium <francium@francium.cc>
2019-01-10 21:16:32 -0500

    Lint fix

--- src/components/graph/types.ts
+++ src/components/graph/types.ts
@@ -1,4 +1,4 @@
-import {IGraphNodeData, GraphNodeId, IPosition} from '@lib/types'
+import {GraphNodeId, IGraphNodeData, IPosition} from '@lib/types'
 
 export const NODE_CLICK_TYPE = 'nodeClick'
 type NodeClickType           = 'nodeClick'
