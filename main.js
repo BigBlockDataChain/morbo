b712c051322d7db861498e8a0cdcf1b2112ad7ff
francium <francium@francium.cc>
2019-01-13 00:04:15 -0500

    Remove Darwin specific code from electron launcher
    Current there doesn't seem to be any real benefits from having the
    application not shutdown completely on Darwin. There has also been
    reported issues as a result of the application not shutting down
    completely during development on Drawin platforms.

--- main.js
+++ main.js
@@ -6,26 +6,14 @@ const {
   REDUX_DEVTOOLS,
 } = require('electron-devtools-installer');
 
-let win = null
-
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar to stay active
-  // until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin')
-    app.quit()
-})
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the dock icon is
-  // clicked and there are no other windows open.
-  if (win === null)
-    createWindow()
+  app.quit()
 })
 
 function createWindow() {
-  win = new BrowserWindow({width: 1024, height: 600})
+  const win = new BrowserWindow({width: 1024, height: 600})
   const indexPath = url.format({
     protocol: 'http:',
     host: 'localhost:8080',
@@ -33,10 +21,6 @@ function createWindow() {
     slashes: true
   })
   win.loadURL(indexPath)
-
-  win.on('closed', () => {
-    win = null
-  })
 
   if (process.env.NODE_ENV === 'PRODUCTION') {
     ipcMain.on('app_quit', (event, info) => {
