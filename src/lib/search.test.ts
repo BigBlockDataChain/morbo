4cd5524f8d2db105b24d108efbc7766e0cdbf2d9
francium <francium@francium.cc>
2019-01-18 14:44:19 -0500

    Update search module tests

--- src/lib/search.test.ts
+++ src/lib/search.test.ts
@@ -4,48 +4,75 @@ import {IGraphMetadata} from './types'
 const mockMetadata: IGraphMetadata = {
   1: {
     id: 1,
-    title: 'One',
+    title: 'File One',
     lastModified: '',
     created: '',
     x: 12,
     y: 12,
-    tags: ['tagA'],
+    tags: ['alpha', 'gamma'],
   },
   2: {
     id: 2,
-    title: 'Two',
+    title: 'Important File Two',
     lastModified: '',
     created: '',
     x: 12,
     y: 12,
-    tags: ['tagB'],
+    tags: ['beta'],
   },
   3: {
     id: 3,
-    title: 'Three',
+    title: 'Important File Three',
     lastModified: '',
     created: '',
     x: 12,
     y: 12,
-    tags: ['tagC'],
+    tags: ['charlie', 'gamma'],
   },
 }
 
-test('no results', () => {
-  return expect(search(mockMetadata, 'foo')).resolves.toEqual([])
+describe('title', () => {
+
+  test('no results', () => {
+    return expect(search(mockMetadata, 'foo')).resolves.toEqual([])
+  })
+
+  test('one result', () => {
+    return search(mockMetadata, 'File two').then((results: any) => {
+      expect(results.length).toBe(1)
+      expect(results[0].title).toBe(mockMetadata[2].title)
+    })
+  })
+
+  test('multiple results', () => {
+    return search(mockMetadata, 'Important File').then((results: any) => {
+      expect(results.length).toBe(2)
+      expect(results[0].title).toBe(mockMetadata[2].title)
+      expect(results[1].title).toBe(mockMetadata[3].title)
+    })
+  })
+
 })
 
-test('one result', () => {
-  return search(mockMetadata, 'two').then((results: any) => {
-    expect(results.length).toBe(1)
-    expect(results[0].title).toBe('Two')
-  })
-})
+describe('tags', () => {
 
-test('multiple results', () => {
-  return search(mockMetadata, 'e').then((results: any) => {
-    expect(results.length).toBe(2)
-    expect(results[0].title).toBe('One')
-    expect(results[1].title).toBe('Three')
+  test('no results', () => {
+    return expect(search(mockMetadata, 'delta')).resolves.toEqual([])
   })
+
+  test('one result', () => {
+    return search(mockMetadata, 'alpha').then((results: any) => {
+      expect(results.length).toBe(1)
+      expect(results[0].title).toBe(mockMetadata[1].title)
+    })
+  })
+
+  test('multiple results', () => {
+    return search(mockMetadata, 'gamma').then((results: any) => {
+      expect(results.length).toBe(2)
+      expect(results[0].title).toBe(mockMetadata[1].title)
+      expect(results[1].title).toBe(mockMetadata[3].title)
+    })
+  })
+
 })
