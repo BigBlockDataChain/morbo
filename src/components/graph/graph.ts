c8c95ab7f7cc99f1bf813a2086a1e56076169ad6
miktime148 <miktime@hotmail.com>
2019-03-19 09:08:43 -0400

    Fix lint error

--- src/components/graph/graph.ts
+++ src/components/graph/graph.ts
@@ -428,7 +428,7 @@ export default class GraphComponent {
 
       nodeCircles[nodeId] = intersectLineWithRectange(line, rect)
     })
-    
+
     const visibleNodes = this._visibleNodeMetadata()
 
     const newNodes = existingNodes
