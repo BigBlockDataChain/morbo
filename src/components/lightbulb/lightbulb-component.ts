1565b421aff2f370da19d2da9be688410bb1d54f
jay dong <daodanhongni@gmail.com>
2019-03-31 17:31:14 -0400

    added user manual

--- src/components/lightbulb/lightbulb-component.ts
+++ src/components/lightbulb/lightbulb-component.ts
@@ -30,6 +30,30 @@ export function view(
             {class: 'lightbulb-title'},
             'Do you know?',
           ),
+          html.div(
+            'To create a new note: right mouse click on home screen -> "New Note"'
+          ),
+          html.div(
+            'To edit an existing note: right mouse click on that note -> "Edit" OR double click the note'
+          ),
+          html.div(
+            'To create a child node of an existing note: right mouse click on that note -> "New child note"'
+          ),
+          html.div(
+            'To assign a parent note to a child note: right mouse click on the parent note -> "Make parent of"'
+          ),
+          html.div(
+            'To set a note as home: right mouse that note -> "Set here as home"'
+          ),
+          html.div(
+            'To place a note at the center of screen: right mouse click on that note -> "Center screen"'
+          ),
+          html.div(
+            'To import/export a working environment: click the "Gear" button -> import/export'
+          ),
+          html.div(
+            'Toggle dark theme: click the "Gear" button -> "Dark Theme"'
+          ),
         ],
       ),
     ],
